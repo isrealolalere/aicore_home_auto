c3472d9a5def5efd79244a7f364db1b135ef5ac6
ISRAEL <isrealolalere09@gmail.com>
2024-05-27 17:26:32 +0100

    first commit

---
+++ aicore_web/static/js/scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navToggle = document.getElementById("nav-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    navToggle.addEventListener("click", function() {
+        navMenu.classList.toggle("show");
+        console.log('clicked');
+    });
+});
